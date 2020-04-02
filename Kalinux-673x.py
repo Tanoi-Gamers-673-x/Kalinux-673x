cf94839aae70956195be882540e6fdc7374e01c6
Tanoi-Gamers-673-x <62834829+Tanoi-Gamers-673-x@users.noreply.github.com>
2020-04-02 17:33:48 +0700

    Add files via upload

---
+++ Kalinux-673x.py
@@ -0,0 +1,57 @@
+#Python Progarming By Tanoi Gamers
+#Script Kalinux-673x
+
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+#CodeLoad
+os.system("clear")
+print
+os.system("figlet Kalinux-673x |lolcat")
+print "\033[1m\033[32mPython Programing By Tanoi Gamers"
+print "\033[0m"
+print "[                    ] 0% "
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
+
+A = raw_input("Please press enter to continue.[TN]>")
+
+#Code About Kalinux-673x
+os.system("clear")
+os.system("figlet Kalinux-673x |lolcat")
+os.system("pkg install sl -y")
+os.system("sl")
+os.system("cd")
+os.system("apt-get update -y")
+os.system("pkg install wget -y")
+os.system("pkg install python -y")
+os.system("pkg install python2 -y")
+os.system("pkg install openssh -y")
+os.system("pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh")
+os.system("ls")
+os.system("cd Kalinux-673x")
+os.system("cp kalinux.txt $HOME")
+os.system("ls")
+os.system("cd")
+os.system("ls")
+os.system("claer")
+os.system("rm kalinux.txt")
+os.system("./start-kali.sh")
+#End
---
+++ README.md
@@ -0,0 +1,32 @@
+@Python Programing By Tanoi Gamers
+
+@Kalinux-673x is available for
+
+@Termux
+
+:Installation and usage guide
+
+$ apt-get update -y
+
+$ apt-get upgrade -y
+
+$ pkg install git -y
+
+$ git clone https://github.com/Tanoi-Gamers-673-x/Kalinux-673x
+
+$ ls
+
+$ cd Kalinux-673x
+
+$ ls
+
+$ python2 Kalinux-673x.py
+
+####################
+# ./start-kali.sh  #
+####################
+
+####################
+# vncserver-start  #
+####################
+
